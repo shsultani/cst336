b4c29cd3abb6ca2d0859be01cfa7660b20f20d47
Shaikh <ssultani@csumb.edu>
2017-09-05 20:55:49 +0000

    lab link

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Heroku!';
-
-?>
+<a href="personalportfolio/index.html">Lab 1</a>
 </body>
 </html>
